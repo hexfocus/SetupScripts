5277b4d69916382df1ba046e888ae5a1de3aacbd
Devops <devops@host>
2020-11-08 15:18:37 +0100

    virtualbox

---
+++ linux/virtualbox/install_virtualbox.sh
@@ -0,0 +1,7 @@
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | \
+    sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+
+sudo apt-get update
+sudod apt-get install virtualbox-6.1
